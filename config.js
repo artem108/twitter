f8514b43506c94eecbae166566766c9cee269f10
artem108 <artem.arshaniy108@gmail.com>
2018-03-18 23:53:10 +0200

    Install Twit, got api keys and connect to Twitter

---
+++ config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  consumer_key:         'SgE7rYBcTyjOEHCi2a3Lnk4sv',
+  consumer_secret:      'dhraQRowtmJ3R6S5QgmuQPd97dGgMaUxVS713kEsHQRkiFlGxT',
+  access_token:         '846059230545227778-WtxQeqDZQWGGVDe0WdCZDkKKhIJvlhN',
+  access_token_secret:  'zqjFh17OGVYNoYOjDsuCzwhyQMKIc4Ixzej6nQ4jeb84l',
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+var Twit = require('twit')
+var config = require('../config')
+
+var T = new Twit(config)
+
+var params = {
+    q: '#rainbow',
+    count: 5
+  }
+
+function getData(err, data, response) {
+  var tweets = data.statuses
+  for (var i = 0; i < tweets.length; i++) {
+    console.log(tweets[i].entities.hashtags)
+    console.log('____________');
+  }
+}
+T.get('search/tweets', params, getData)
+
+
+// var tweet = {
+//   status: ''
+// }
+// function tweeted(err, data, response) {
+// console.log(data);
+// }
+// T.post('statuses/update', tweet, tweeted)
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
